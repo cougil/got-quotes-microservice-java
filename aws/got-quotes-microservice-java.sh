39ee0554b9587642f5e1216deb6c2b44d1a184fb
Roger Solé <roger.sole@schibsted.com>
2015-09-04 10:55:29 +0200

    initial commit

---
+++ aws/got-quotes-microservice-java.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+sudo mkdir /opt/got-quotes-microservice-java
+sudo tar -xvf /opt/got-quotes-microservice-java.tar -C /opt/got-quotes-microservice-java
+sudo mv /opt/got-quotes-microservice-java/got-quotes-microservice-java*/* /opt/got-quotes-microservice-java
+
+sudo chown -R root:root /opt/got-quotes-microservice-java
+sudo chmod -R 644 /opt/got-quotes-microservice-java
+
+sudo chown -R root:root /opt/got-quotes-microservice-java
+sudo chmod 755 /opt/got-quotes-microservice-java/bin/got-quotes-microservice-java
+
+sudo mkdir /var/log/got-quotes-microservice-java
+
+sudo bash -c "echo tags:got_quotes_microservice_java >> /etc/dd-agent/datadog.conf"
